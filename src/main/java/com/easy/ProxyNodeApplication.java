094ba4949b0ef3a1c0b249b20e065ccb4dd11a2f
2190418744@qq.com <2190418744@qq.com>
2024-12-14 02:48:48 +0800

    first_submit

---
+++ src/main/java/com/easy/ProxyNodeApplication.java
@@ -0,0 +1,34 @@
+package com.easy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ProxyNodeApplication {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ProxyNodeApplication.class);
+
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ProxyNodeApplication.class);
+        ConfigurableApplicationContext run = app.run(args);
+        Environment env = run.getEnvironment();
+        String severPort = env.getProperty("server.port");
+        String logo = """
+                ██████╗ ██████╗  ██████╗ ██╗  ██╗██╗   ██╗███╗   ██╗ ██████╗ ██████╗ ███████╗
+                ██╔══██╗██╔══██╗██╔═══██╗╚██╗██╔╝╚██╗ ██╔╝████╗  ██║██╔═══██╗██╔══██╗██╔════╝
+                ██████╔╝██████╔╝██║   ██║ ╚███╔╝  ╚████╔╝ ██╔██╗ ██║██║   ██║██║  ██║█████╗ \s
+                ██╔═══╝ ██╔══██╗██║   ██║ ██╔██╗   ╚██╔╝  ██║╚██╗██║██║   ██║██║  ██║██╔══╝ \s
+                ██║     ██║  ██║╚██████╔╝██╔╝ ██╗   ██║   ██║ ╚████║╚██████╔╝██████╔╝███████╗
+                ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝\s
+                PROFILE: %s
+                SERVER PORT: %s""";
+        LOGGER.warn("\n" + String.format(logo, Arrays.toString(env.getActiveProfiles()), severPort));
+    }
+}
---
+++ src/test/java/com/easy/proxy_node/ProxyNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.proxy_node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyNodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
